6bd8d58164938e1d5fa4ae7aedfed524470e9e55
officialrohitsharma <rohitsharmatech9@gmail.com>
2023-11-20 17:33:08 +0530

    minor updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import homebg from "../assets/HomePage.png"; // Assuming homebg.jpg is the correct path to your image
 import { Link } from "react-router-dom";
 const Home = () => {
