3764a715ba2c60dd3b1c46368c9b11a3c43bce25
officialrohitsharma <rohitsharmatech9@gmail.com>
2023-11-25 04:10:21 +0530

    complete

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -133,7 +133,7 @@ const Login = () => {
                 Github
               </button>
               <button
-                className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                className="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                 type="button"
               >
                 <img
